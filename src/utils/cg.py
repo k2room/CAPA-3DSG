a4ecd56ac131f33b45ef2607915e26decd24257f
k2room <kimyirum@gm.gist.ac.kr>
2025-09-15 16:26:35 +0900

    [FEAT] thirdparty utils, CAPA3DSG utils

---
+++ src/utils/cg.py
@@ -0,0 +1,70 @@
+from conceptgraph.utils.optional_rerun_wrapper import (
+    OptionalReRun, 
+    orr_log_annotated_image, 
+    orr_log_camera, 
+    orr_log_depth_image, 
+    orr_log_edges, 
+    orr_log_objs_pcd_and_bbox, 
+    orr_log_rgb_image, 
+    orr_log_vlm_image
+)
+from conceptgraph.utils.optional_wandb_wrapper import OptionalWandB
+from conceptgraph.utils.geometry import rotation_matrix_to_quaternion
+from conceptgraph.utils.logging_metrics import DenoisingTracker, MappingTracker
+from conceptgraph.utils.vlm import consolidate_captions, get_obj_rel_from_image_gpt4v, get_openai_client
+from conceptgraph.utils.ious import mask_subtract_contained
+from conceptgraph.utils.general_utils import (
+    ObjectClasses, 
+    find_existing_image_path, 
+    get_det_out_path, 
+    get_exp_out_path, 
+    get_vlm_annotated_image_path, 
+    handle_rerun_saving, 
+    load_saved_detections, 
+    load_saved_hydra_json_config, 
+    make_vlm_edges_and_captions, 
+    measure_time, 
+    save_detection_results,
+    save_edge_json, 
+    save_hydra_config,
+    save_obj_json, 
+    save_objects_for_frame, 
+    save_pointcloud, 
+    should_exit_early, 
+    vis_render_image
+)
+from conceptgraph.dataset.datasets_common import get_dataset
+from conceptgraph.utils.vis import (
+    OnlineObjectRenderer, 
+    save_video_from_frames, 
+    vis_result_fast_on_depth, 
+    vis_result_for_vlm, 
+    vis_result_fast, 
+    save_video_detections
+)
+from conceptgraph.slam.slam_classes import MapEdgeMapping, MapObjectList
+from conceptgraph.slam.utils import (
+    filter_gobs,
+    filter_objects,
+    get_bounding_box,
+    init_process_pcd,
+    make_detection_list_from_pcd_and_gobs,
+    denoise_objects,
+    merge_objects, 
+    detections_to_obj_pcd_and_bbox,
+    prepare_objects_save_vis,
+    process_cfg,
+    process_edges,
+    process_pcd,
+    processing_needed,
+    resize_gobs
+)
+from conceptgraph.slam.mapping import (
+    compute_spatial_similarities,
+    compute_visual_similarities,
+    aggregate_similarities,
+    match_detections_to_objects,
+    merge_obj_matches
+)
+from conceptgraph.utils.model_utils import compute_clip_features_batched
+from conceptgraph.utils.general_utils import get_vis_out_path, cfg_to_dict, check_run_detections
---
+++ src/utils/gsa.py
@@ -0,0 +1,5 @@
+from groundingdino.util.inference import Model as GDINO
+from segment_anything import sam_model_registry, SamPredictor
+from ram.models import ram as RAM
+from ram import inference_ram
+
